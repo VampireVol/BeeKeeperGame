0dbd47be95716185ea90367a9ff62e922b2b585e
VampireVol <sergeyb11@ya.ru>
2020-12-14 22:32:13 +0300

    start menu fixing

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Color32 tabIdle;
+    public Color32 tabHover;
+    public Color32 tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+    
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEneter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.color = tabHover;
+        }        
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.background.color = tabActive;
+        int index = button.transform.GetSiblingIndex() - 1;
+        for (int i = 0; i < objectsToSwap.Count; ++i)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab)
+                continue;
+            button.background.color = tabIdle;
+        }
+    }
+}
